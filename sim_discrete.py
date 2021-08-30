eecb2667160418dc278e9e02abe8fed02424d481
you lin qing <595849292@qq.com>
2021-08-30 16:08:52 +0800

    Add files via upload

--- sim_discrete.py
+++ sim_discrete.py
@@ -19,13 +19,18 @@ f_thetal_q_l = 5 / 7
 
 
 """
-f_thetah_q_h = 6.7/7#4.7,6.7,6.7/7
-f_thetah_q_l = 1-f_thetah_q_h
-f_thetal_q_h =f_thetah_q_l
+f_thetah_q_h = 4 / 7  # 4.7,6.7,6.7/7
+f_thetah_q_l = 1 - f_thetah_q_h
+f_thetal_q_h = f_thetah_q_l
 f_thetal_q_l = f_thetah_q_h
 
 
-def f(alpha_A):  # r_{L}
+# ========================================================
+# this is the expected return if:
+# (1) firm conducts short-term project
+# (2) investor has a belief on long-term project when CSR is observed
+# ==========================================================
+def f(alpha_A):  # r_{S}
     aa = u_H * alpha_A * f_thetah_q_h * f_thetah_q_l
     cc = alpha_A * f_thetah_q_h + (1 - alpha_A) * f_thetah_q_l
     bb = u_H * alpha_A * f_thetal_q_h * f_thetal_q_l
@@ -33,7 +38,12 @@ def f(alpha_A):  # r_{L}
     return aa / cc + bb / dd
 
 
-def ff(alpha_A):  # r_{H}
+# ========================================================
+# this is the expected return if:
+# (1) firm conducts long-term project
+# (2) investor has a belief on long-term project when CSR is observed
+# ==========================================================
+def ff(alpha_A):  # r_{L}
     aa = u_H * alpha_A * f_thetah_q_h * f_thetah_q_h
     cc = alpha_A * f_thetah_q_h + (1 - alpha_A) * f_thetah_q_l
     bb = u_H * alpha_A * f_thetal_q_h * f_thetal_q_h
@@ -41,7 +51,14 @@ def ff(alpha_A):  # r_{H}
     return aa / cc + bb / dd
 
 
-def m(alpha_B):  # r_{L}
+# ========================================================
+# this is the expected return if:
+# (1) firm conducts short-term project
+# (2) investor has a belief for long-term project when non-CSR activity
+#     is observed
+# ==========================================================
+
+def m(alpha_B):  # r_{S}
     aa = u_H * alpha_B * f_thetah_q_h * f_thetah_q_l
     cc = alpha_B * f_thetah_q_h + (1 - alpha_B) * f_thetah_q_l
     bb = u_H * alpha_B * f_thetal_q_h * f_thetal_q_l
@@ -49,7 +66,14 @@ def m(alpha_B):  # r_{L}
     return aa / cc + bb / dd
 
 
-def mm(alpha_B):  # r_{H}
+# ========================================================
+# this is the expected return if:
+# (1) firm conducts long-term project
+# (2) investor has a belief for long-term project when non-CSR activity
+#     is observed
+# ==========================================================
+
+def mm(alpha_B):  # r_{L}
     aa = u_H * alpha_B * f_thetah_q_h * f_thetah_q_h
     cc = alpha_B * f_thetah_q_h + (1 - alpha_B) * f_thetah_q_l
     bb = u_H * alpha_B * f_thetal_q_h * f_thetal_q_h
@@ -77,67 +101,63 @@ def test(x, y, la):
 
 # Non-CSR pooling
 def non_csr_pooling():
-    alpha_A_list = np.linspace(0.01, la, 500)
+    alpha_A_list = np.linspace(0, 0.99, 500)
+    alphaB=0
+    rSB = m(alphaB)
     result = []
     for alpha_A in alpha_A_list:
-        rH = ff(alpha_A)
-        rL = f(alpha_A)
-        if rL < c_a and rH < c_a + c_l:
+        rSA = f(alpha_A)
+        rLA = ff(alpha_A)
+        if rSA-c_a < rSB and rLA-c_a-c_l < rSB:
             result.append(alpha_A)
     if len(result) != 0:
-        #print(['alphab', result])
-        print(['Noc-CSR pooling', 'alphaB=0', 'alpha_A is in [{},{}]'.format(str(min(result)), str(max(result)))])
-    alpha_B_list = np.linspace(0, 0.99, 500)
-    result1 = []
-    for alpha_B in alpha_B_list:
-        rH = mm(alpha_B)
-        rL = m(alpha_B)
-        if rL >= rH - c_l:
-            result1.append(alpha_B)
-    if len(result1) != 0:
-        #print(['alphab',result1])
-        print(['Noc-CSR pooling', 'alphaA=0', 'alpha_B is in [{},{}]'.format(str(min(result1)), str(max(result1)))])
+        # print(['alphab', result])
+        print(['Non-CSR pooling', 'alphaB=0', 'alpha_A is in [{},{}]'.format(str(min(result)), str(max(result)))])
 
 
 #
 def non_csr_separating():
     alphB = la
-    rLB = m(alphB)
-    rHB = m(alphB)
-    alpha_A_list = np.linspace(0, la - 0.001, 500)
+    rSB = m(alphB)
+    alpha_A_list = np.linspace(0, 0.99, 500)
     result = []
     for alpha_A in alpha_A_list:
-        rHA = ff(alpha_A)
-        rLA = f(alpha_A)
-        if rLA - c_a < rLB and rHB > rHA - c_a and rHB - c_l > rLB:
+        rSA = f(alpha_A)
+        if rSA - c_a < rSB:
             result.append(alpha_A)
     if len(result) != 0:
         print(['Non-CSR separating', 'alphaB={}'.format(str(la)),
                'alpha_A is in [{},{}]'.format(str(min(result)), str(max(result)))])
 
+
 def csr_hybrid():
-    alpha_A_list = np.linspace(0.01+la, 0.999, 500)
+    alpha_A_list = np.linspace(0.01 + la, 0.99, 500)
     result = []
     for alpha_A in alpha_A_list:
-        rH = ff(alpha_A)
-        rL = f(alpha_A)
-        if abs(rL- c_a)<0.01 and rH-rL >c_l:
+        # rH = ff(alpha_A)
+        rS = f(alpha_A)
+        if abs(rS - c_a) < 0.01:
             result.append(alpha_A)
     if len(result) != 0:
         print(['CSR hybrid', 'alphaB=0', 'alpha_A is in [{},{}]'.format(str(min(result)), str(max(result)))])
 
-def csr_separating():
-    alpha_B_list = np.linspace(0, la, 500)
-    rLA=f(la)
-    rHA=ff(la)
-    result = []
+
+def csr_pooling():
+    alpha_B_list = np.linspace(0.01, 0.99, 500)
+    alpha_A_list = np.linspace(0.01, 0.99, 500)
+    result_B = []
+    result_A = []
     for alpha_B in alpha_B_list:
-        #rH = ff(alpha_A)
-        rLB = m(alpha_B)
-        if rLA-c_a>rLB and rHA-rLA>c_l:
-            result.append(alpha_B)
-    if len(result) != 0:
-        print(['CSR separating', 'alphaA={}'.format(str(la)), 'alpha_B is in [{},{}]'.format(str(min(result)), str(max(result)))])
+        for alpha_A in alpha_A_list:
+            rSA = f(alpha_A)
+            rSB = m(alpha_B)
+            if abs(rSA-c_a-rSB)<0.01:
+                result_B.append(alpha_B)
+                result_A.append(alpha_A)
+    if len(result_A) != 0:
+        print(['CSR pooling', 'alpha_A is in [{},{}]'.format(str(min(result_A)), str(max(result_A))),
+               'alpha_B is in [{},{}]'.format(str(min(result_B)), str(max(result_B)))])
+
 
 # la = 0.2
 # b = ff(alpha_A=la)
@@ -148,5 +168,4 @@ def csr_separating():
 non_csr_pooling()
 non_csr_separating()
 csr_hybrid()
-#csr_separating()
-
+csr_pooling()
